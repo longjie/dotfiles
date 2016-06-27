f002db000e7e7319a14c35bf3b70a95cb051703a
Ryosuke Tajima <ryosuke.tajima@gmail.com>
2016-06-27 22:05:40 +0900

    remove emacs scroll bar

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -169,3 +169,15 @@
 ;; Octave mode
 (require 'ocrave-mode nil t)
 (add-to-list 'auto-mode-alist '("\\.m\\'" . octave-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(scroll-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
